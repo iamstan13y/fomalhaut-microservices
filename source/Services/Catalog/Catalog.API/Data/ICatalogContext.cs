2519b416a537f51c208aa4881e6acf27f970cb2d
iamstan13y <iamstan13y@gmail.com>
2021-11-06 23:45:22 +0200

    Data Layer - Connect Mongo Docker Container from Catalog.API

---
+++ source/Services/Catalog/Catalog.API/Data/ICatalogContext.cs
@@ -0,0 +1,14 @@
+﻿using Catalog.API.Models;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Catalog.API
+{
+    public interface ICatalogContext
+    {
+        IMongoCollection<Product> Products { get; }
+    }
+}
