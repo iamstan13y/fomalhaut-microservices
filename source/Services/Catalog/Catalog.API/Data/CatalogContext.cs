8659ea53b747a81843f094b4abce6bb55ab95a57
iamstan13y <iamstan13y@gmail.com>
2021-11-08 10:41:16 +0200

    Catalog Context Seed Products.

--- source/Services/Catalog/Catalog.API/Data/CatalogContext.cs
+++ source/Services/Catalog/Catalog.API/Data/CatalogContext.cs
@@ -16,7 +16,7 @@ namespace Catalog.API.Data
             var database = client.GetDatabase(configuration.GetValue<string>("DatabaseSettings:DatabaseName"));
 
             Products = database.GetCollection<Product>(configuration.GetValue<string>("DatabaseSettings:CollectionName"));
-            // CatalogContextSeed.SeedData(Products);
+            CatalogContextSeed.SeedData(Products);
         
         }
 
