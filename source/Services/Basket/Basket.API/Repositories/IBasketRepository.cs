9cb38d927e347273a90af72362f200dc4de008e9
iamstan13y <iamstan13y@gmail.com>
2021-11-10 14:54:29 +0200

    Repository Pattern on Basket.API Service.

---
+++ source/Services/Basket/Basket.API/Repositories/IBasketRepository.cs
@@ -0,0 +1,15 @@
+﻿using Basket.API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Basket.API.Repositories
+{
+    public interface IBasketRepository
+    {
+        Task<ShoppingCart> GetBasket(string username);
+        Task<ShoppingCart> UpdateBasket(ShoppingCart basket);
+        Task DeleteBasket(string username);
+    }
+}
