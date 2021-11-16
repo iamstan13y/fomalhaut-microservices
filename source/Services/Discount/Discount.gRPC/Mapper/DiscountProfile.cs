df8b4c400dc122715728e61e42ea0dc06620670a
iamstan13y <iamstan13y@gmail.com>
2021-11-16 15:50:15 +0200

    Code clean-up in Discount.gRPC

--- source/Services/Discount/Discount.gRPC/Mapper/DiscountProfile.cs
+++ source/Services/Discount/Discount.gRPC/Mapper/DiscountProfile.cs
@@ -1,10 +1,6 @@
 ﻿using AutoMapper;
 using Discount.gRPC.Models;
 using Discount.gRPC.Protos;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Discount.gRPC.Mapper
 {
--- source/Services/Discount/Discount.gRPC/Services/DiscountService.cs
+++ source/Services/Discount/Discount.gRPC/Services/DiscountService.cs
@@ -5,8 +5,6 @@ using Discount.gRPC.Repositories;
 using Grpc.Core;
 using Microsoft.Extensions.Logging;
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace Discount.gRPC.Services
