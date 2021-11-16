b2adf2065fe2a6b995340808d49c2eb447780638
iamstan13y <iamstan13y@gmail.com>
2021-11-16 12:11:23 +0200

    Developing discount.proto ProtoBuf file for Exposing CRUD Services Discount.gRPC

--- source/Services/Discount/Discount.gRPC/Startup.cs
+++ source/Services/Discount/Discount.gRPC/Startup.cs
@@ -33,7 +33,7 @@ namespace Discount.gRPC
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGrpcService<GreeterService>();
+                //endpoints.MapGrpcService<GreeterService>();
 
                 endpoints.MapGet("/", async context =>
                 {
