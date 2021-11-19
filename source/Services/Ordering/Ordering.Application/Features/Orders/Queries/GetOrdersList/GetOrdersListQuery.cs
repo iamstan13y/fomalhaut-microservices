f6a84015b81ffc05bf7a4f2e850626a401dff17d
iamstan13y <iamstan13y@gmail.com>
2021-11-19 15:57:46 +0200

    Ordering.Application Layer- Application Features - GetOrdersListQuery.

---
+++ source/Services/Ordering/Ordering.Application/Features/Orders/Queries/GetOrdersList/GetOrdersListQuery.cs
@@ -0,0 +1,19 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Application.Features.Orders.Queries.GetOrdersList
+{
+    public class GetOrdersListQuery : IRequest<List<OrdersVm>>
+    {
+        public string Username { get; set; }
+
+        public GetOrdersListQuery(string username)
+        {
+            Username = username ?? throw new ArgumentNullException(nameof(username));
+        }
+    }
+}
--- source/Services/Ordering/Ordering.Application/Mappings/MappingProfile.cs
+++ source/Services/Ordering/Ordering.Application/Mappings/MappingProfile.cs
@@ -1,4 +1,6 @@
 ﻿using AutoMapper;
+using Ordering.Application.Features.Orders.Queries.GetOrdersList;
+using Ordering.Domain.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,5 +11,9 @@ namespace Ordering.Application.Mappings
 {
     public class MappingProfile : Profile
     {
+        public MappingProfile()
+        {
+            CreateMap<Order, OrdersVm>().ReverseMap();
+        }
     }
 }
