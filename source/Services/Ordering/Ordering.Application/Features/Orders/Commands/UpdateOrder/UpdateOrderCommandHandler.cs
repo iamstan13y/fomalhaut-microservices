9399278367eaa2bbcb23f2d4c6554f8768758e4d
iamstan13y <iamstan13y@gmail.com>
2021-11-22 08:41:17 +0200

    Ordering.Application Layer- Application Command Features- UpdateOrder - I.

---
+++ source/Services/Ordering/Ordering.Application/Features/Orders/Commands/UpdateOrder/UpdateOrderCommandHandler.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using MediatR;
+using Microsoft.Extensions.Logging;
+using Ordering.Application.Contracts.Persistence;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Ordering.Application.Features.Orders.Commands.UpdateOrder
+{
+    public class UpdateOrderCommandHandler : IRequestHandler<UpdateOrderCommand>
+    {
+        private readonly IOrderRepository _orderRepository;
+        private readonly IMapper _mapper;
+        private readonly ILogger<UpdateOrderCommandHandler> _logger;
+        public Task<Unit> Handle(UpdateOrderCommand request, CancellationToken cancellationToken)
+        {
+
+        }
+    }
+}
