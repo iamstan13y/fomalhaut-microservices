9a062903e217f600014755d922e9f73780f790ca
iamstan13y <iamstan13y@gmail.com>
2021-11-23 08:43:43 +0200

    Ordering.Application Layer- Application Command Features- UpdateOrder - I.

--- source/Services/Ordering/Ordering.Application/Features/Orders/Commands/UpdateOrder/UpdateOrderCommandHandler.cs
+++ source/Services/Ordering/Ordering.Application/Features/Orders/Commands/UpdateOrder/UpdateOrderCommandHandler.cs
@@ -2,6 +2,7 @@
 using MediatR;
 using Microsoft.Extensions.Logging;
 using Ordering.Application.Contracts.Persistence;
+using Ordering.Domain.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -16,9 +17,21 @@ namespace Ordering.Application.Features.Orders.Commands.UpdateOrder
         private readonly IOrderRepository _orderRepository;
         private readonly IMapper _mapper;
         private readonly ILogger<UpdateOrderCommandHandler> _logger;
-        public Task<Unit> Handle(UpdateOrderCommand request, CancellationToken cancellationToken)
+        public async Task<Unit> Handle(UpdateOrderCommand request, CancellationToken cancellationToken)
         {
+            var orderToUpdate = await _orderRepository.GetByIdAsync(request.Id);
+            if(orderToUpdate == null)
+            {
+                _logger.LogError("Order not found in database.");
+            }
 
+            _mapper.Map(request, orderToUpdate, typeof(UpdateOrderCommand), typeof(Order));
+
+            await _orderRepository.UpdateAsync(orderToUpdate);
+
+            _logger.LogInformation($"Order {orderToUpdate.Id} is successfully updated.");
+
+            return Unit.Value;
         }
     }
 }
