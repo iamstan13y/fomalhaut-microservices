d9156a3ce711e9c90e32053ddd78315d12b8fc58
iamstan13y <iamstan13y@gmail.com>
2021-11-27 21:36:24 +0200

    Ordering.Infrastructure Layer in Clean Architecture - Persistence - I.

---
+++ source/Services/Ordering/Ordering.Infrastructure/Repositories/RepositoryBase.cs
@@ -0,0 +1,15 @@
+﻿using Ordering.Application.Contracts.Persistence;
+using Ordering.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Infrastructure.Repositories
+{
+    public class RepositoryBase<T> : IAsyncRepository<T> where T : EntityBase
+    {
+
+    }
+}
