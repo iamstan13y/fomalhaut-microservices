fb19f86a3ed3eafa57f83e731de3477ea27353ba
iamstan13y <iamstan13y@gmail.com>
2021-12-01 03:25:50 +0200

    Code clean-up in Ordering.API

--- source/Services/Ordering/Ordering.API/Controllers/OrderController.cs
+++ source/Services/Ordering/Ordering.API/Controllers/OrderController.cs
@@ -7,7 +7,6 @@ using Ordering.Application.Features.Orders.Commands.UpdateOrder;
 using Ordering.Application.Features.Orders.Queries.GetOrdersList;
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Net;
 using System.Threading.Tasks;
 
--- source/Services/Ordering/Ordering.API/Program.cs
+++ source/Services/Ordering/Ordering.API/Program.cs
@@ -1,14 +1,9 @@
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Ordering.API.Extensions;
 using Ordering.Infrastructure.Persistence;
 using Microsoft.Extensions.DependencyInjection;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Ordering.API
 {
