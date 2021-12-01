d4cda4eb34433061e850476b8559199b5f74acc1
iamstan13y <iamstan13y@gmail.com>
2021-12-01 02:35:05 +0200

    Testing & Running Ordering.API - ILogger fix.

--- source/Services/Ordering/Ordering.Application/Behaviours/UnhandledExceptionBehaviour.cs
+++ source/Services/Ordering/Ordering.Application/Behaviours/UnhandledExceptionBehaviour.cs
@@ -8,9 +8,9 @@ namespace Ordering.Application.Behaviours
 {
     public class UnhandledExceptionBehaviour<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
     {
-        private readonly ILogger _logger;
+        private readonly ILogger<UnhandledExceptionBehaviour<TRequest, TResponse>> _logger;
 
-        public UnhandledExceptionBehaviour(ILogger logger)
+        public UnhandledExceptionBehaviour(ILogger<UnhandledExceptionBehaviour<TRequest, TResponse>> logger)
         {
             _logger = logger ?? throw new ArgumentNullException(nameof(logger));
         }
--- source/Services/Ordering/Ordering.Infrastructure/Persistence/OrderContext.cs
+++ source/Services/Ordering/Ordering.Infrastructure/Persistence/OrderContext.cs
@@ -16,7 +16,7 @@ namespace Ordering.Infrastructure.Persistence
 
         public DbSet<Order> Orders { get; set; }
 
-        public override Task<int> SaveChangesAsync(bool acceptAllChangesOnSuccess, CancellationToken cancellationToken = default)
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = new CancellationToken())
         {
             foreach(var entry in ChangeTracker.Entries<EntityBase>())
             {
