fd3c854cff2ba792c049fa4ee777d06fcfa30850
iamstan13y <iamstan13y@gmail.com>
2021-12-23 00:37:05 +0200

    Shopping.Web - Index Page.

--- source/WebApps/AspnetRunBasics/Pages/Index.cshtml.cs
+++ source/WebApps/AspnetRunBasics/Pages/Index.cshtml.cs
@@ -4,35 +4,48 @@ using System.Threading.Tasks;
 using AspnetRunBasics.Repositories;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Shopping.Web.Models;
+using Shopping.Web.Services;
 
 namespace AspnetRunBasics.Pages
 {
     public class IndexModel : PageModel
     {
-        private readonly IProductRepository _productRepository;
-        private readonly ICartRepository _cartRepository;
+        private readonly ICatalogService _catalogService;
+        private readonly IBasketService _basketService;
 
-        public IndexModel(IProductRepository productRepository, ICartRepository cartRepository)
+        public IndexModel(ICatalogService catalogService, IBasketService basketService)
         {
-            _productRepository = productRepository ?? throw new ArgumentNullException(nameof(productRepository));
-            _cartRepository = cartRepository ?? throw new ArgumentNullException(nameof(cartRepository));
+            _catalogService = catalogService ?? throw new ArgumentNullException(nameof(catalogService));
+            _basketService = basketService ?? throw new ArgumentNullException(nameof(basketService));
         }
 
-        public IEnumerable<Entities.Product> ProductList { get; set; } = new List<Entities.Product>();
+        public IEnumerable<Catalog> ProductList { get; set; } = new List<Catalog>();
 
         public async Task<IActionResult> OnGetAsync()
         {
-            ProductList = await _productRepository.GetProducts();
+            ProductList = await _catalogService.GetCatalog();
             return Page();
         }
 
-        public async Task<IActionResult> OnPostAddToCartAsync(int productId)
+        public async Task<IActionResult> OnPostAddToCartAsync(string productId)
         {
-            //if (!User.Identity.IsAuthenticated)
-            //    return RedirectToPage("./Account/Login", new { area = "Identity" });
+            var product = await _catalogService.GetCatalog(productId);
 
-            await _cartRepository.AddItem("test", productId);
-            return RedirectToPage("Cart");
+            var basket = await _basketService.GetBasket("juliet");
+
+            basket.Items.Add(new BasketItem
+            {
+                ProductId = product.Id,
+                Color = "Blue",
+                Price = product.Price,
+                Quantity = 1,
+                ProductName = product.Name
+            });
+
+            var updatedBasket = await _basketService.UpdateBasket(basket);
+
+            return RedirectToPage("Cart");      
         }
     }
 }
