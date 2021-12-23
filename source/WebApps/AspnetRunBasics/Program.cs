c377db031d139cc2628c7c99ed365b9931dc4f4f
iamstan13y <iamstan13y@gmail.com>
2021-12-23 02:13:46 +0200

    Shopping.Web - Refactoring Developments.

--- source/WebApps/AspnetRunBasics/Program.cs
+++ source/WebApps/AspnetRunBasics/Program.cs
@@ -15,9 +15,7 @@ namespace AspnetRunBasics
     {
         public static void Main(string[] args)
         {
-            var host = CreateHostBuilder(args).Build();
-            SeedDatabase(host);
-            host.Run();
+            CreateHostBuilder(args).Build().Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
@@ -26,25 +24,5 @@ namespace AspnetRunBasics
                 {
                     webBuilder.UseStartup<Startup>();
                 });
-
-        private static void SeedDatabase(IHost host)
-        {
-            using (var scope = host.Services.CreateScope())
-            {
-                var services = scope.ServiceProvider;
-                var loggerFactory = services.GetRequiredService<ILoggerFactory>();
-
-                try
-                {
-                    var aspnetRunContext = services.GetRequiredService<AspnetRunContext>();
-                    AspnetRunContextSeed.SeedAsync(aspnetRunContext, loggerFactory).Wait();
-                }
-                catch (Exception exception)
-                {
-                    var logger = loggerFactory.CreateLogger<Program>();
-                    logger.LogError(exception, "An error occurred seeding the DB.");
-                }
-            }
-        }
     }
 }
